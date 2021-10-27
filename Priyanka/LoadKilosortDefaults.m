7ca3d06705bf80dd2988943af4af328ff58605eb
priyanka-cshl <speak2priya@gmail.com>
2021-10-27 10:58:21 -0400

    minor edits

--- Priyanka/LoadKilosortDefaults.m
+++ Priyanka/LoadKilosortDefaults.m
@@ -13,7 +13,8 @@ handles.YourConfigFile = '/opt/KiloSort/StandardConfig_Albeanu.m';
 handles.init_from_data = 0; % generate template spikes from data
 handles.spike_det_settings.Data(1) = 2; % x, number of clusters - x times more than Nchan
 handles.spike_det_settings.Data(2) = -4; % spike threshold in standard deviations (4)
-handles.filter2binary.Value = 0; % default is to not save filtered data locally to binary file
+handles.filter2binary.Value = 1; % default is to save bandpassed filtered data locally to binary file
+handles.computeCAR.Value = 1; % default is to subtract the Common average reference
 handles.IgnoreChannels.String = ''; % channels that shouldn't be included in computing CAR
 
 % recording settings
@@ -22,43 +23,19 @@ handles.InactiveChannels.String = ''; % channels that shouldn't be loaded
 
 % overwrite settings as per need
 switch Username
-    case {'standard'}
-    case {'PG32'}
-        handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'}; % Root storage
-        handles.FilePaths.Data(2) = {'J6/'}; % local read/write folder
-        handles.spike_det_settings.Data(1) = 64;
-        handles.filter2binary.Value = 1;
-        handles.YourConfigFile = 'StandardConfig_MARIE_32.m';
-    case {'PG64'}
-        handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'}; % Root storage
-        handles.FilePaths.Data(2) = {'J4/'}; % local read/write folder
-        handles.spike_det_settings.Data(1) = 256;
-        handles.filter2binary.Value = 1;
-        handles.YourConfigFile = 'StandardConfig_PG64.m';
-    case {'PG_APC'}
-        handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'}; % Root storage
-        handles.FilePaths.Data(2) = {'PCX1/'}; % local read/write folder
-        handles.spike_det_settings.Data(1) = 128;
-        handles.filter2binary.Value = 1;
-        handles.YourConfigFile = 'StandardConfig_PG64.m';
     case {'K4'}
         handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'}; % Root storage
-        handles.FilePaths.Data(2) = {'K4/'}; % Animal Name
-        handles.spike_det_settings.Data(1) = 256;
-        handles.filter2binary.Value = 1;
-        handles.YourConfigFile = 'StandardConfig_PG64_K4.m';
-        handles.IgnoreChannels.String = '22 26 41 42 43 45 46 49 50 53 54 55 56 59 60';
+        handles.FilePaths.Data(2) = {'K4'}; % Animal Name
+        handles.IgnoreChannels.String = mat2str([22 26 41 42 43 45 46 49 50 53 54 55 56 59 60]);
     case {'O3'}
         handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'};
-        handles.FilePaths.Data(2) = {'O3/'};
-        handles.FilePaths.Data(3) = {'/mnt/data/Sorted/'}; % local read/write folder
+        handles.FilePaths.Data(2) = {'O3'};
         handles.recording_settings.Data(1) = 64;
         handles.InactiveChannels.String = mat2str(9:32);
         handles.IgnoreChannels.String = mat2str([]);
     case {'MO1'}
         handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'};
-        handles.FilePaths.Data(2) = {'MO1/'};
-        handles.FilePaths.Data(3) = {'/mnt/data/Sorted/'}; % local read/write folder
+        handles.FilePaths.Data(2) = {'MO1'};
         handles.recording_settings.Data(1) = 32;
         handles.InactiveChannels.String = mat2str([]);
         handles.IgnoreChannels.String = mat2str([]);
