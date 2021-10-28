dd7c30e5f53a08c22b0f6b559d43c817c15a31f0
priyanka-cshl <speak2priya@gmail.com>
2021-10-28 13:24:52 -0400

    minor

--- Priyanka/LoadKilosortDefaults.m
+++ Priyanka/LoadKilosortDefaults.m
@@ -6,7 +6,7 @@ addpath(genpath('/opt/npy-matlab/')) % path to npy-matlab scripts
 % default settings
 handles.FilePaths.Data(1) = {'/mnt/data/'}; % Root storage
 handles.FilePaths.Data(2) = {'PCX4'}; % local read/write folder
-handles.FilePaths.Data(2) = {'/mnt/data/Sorted/'}; % local read/write folder
+handles.FilePaths.Data(3) = {'/mnt/data/Sorted/'}; % local read/write folder
 handles.YourConfigFile = '/opt/KiloSort/StandardConfig_Albeanu.m';
 
 % spike detection settings
@@ -27,11 +27,23 @@ switch Username
         handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'}; % Root storage
         handles.FilePaths.Data(2) = {'K4'}; % Animal Name
         handles.IgnoreChannels.String = mat2str([22 26 41 42 43 45 46 49 50 53 54 55 56 59 60]);
-    case {'O3'}
-        handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'};
-        handles.FilePaths.Data(2) = {'O3'};
+    case {'PCX1','PCX3','PCX4','PCX5'}
+        handles.FilePaths.Data(1) = {'/mnt/grid-hs/pgupta/EphysData'};
+        handles.FilePaths.Data(2) = {Username};
+        handles.recording_settings.Data(1) = 64;
+        handles.InactiveChannels.String = mat2str([]);
+        handles.IgnoreChannels.String = mat2str([]);
+    case {'O5','O3'}
+        handles.FilePaths.Data(1) = {'/mnt/grid-hs/mdussauz/ephysdata/lever_task/BatchO'};
+        handles.FilePaths.Data(2) = {Username};
         handles.recording_settings.Data(1) = 64;
         handles.InactiveChannels.String = mat2str(9:32);
+        handles.IgnoreChannels.String = mat2str([]);
+    case {'O2','O1'}
+        handles.FilePaths.Data(1) = {'/mnt/grid-hs/mdussauz/ephysdata/lever_task/BatchO'};
+        handles.FilePaths.Data(2) = {Username};
+        handles.recording_settings.Data(1) = 64;
+        handles.InactiveChannels.String = mat2str(1:32);
         handles.IgnoreChannels.String = mat2str([]);
     case {'MO1'}
         handles.FilePaths.Data(1) = {'/mnt/data/Priyanka'};
