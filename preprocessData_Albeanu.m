5dd81096b3927a02a05c0f5ead625a8ac182584c
priyanka-cshl <speak2priya@gmail.com>
2023-07-20 17:46:23 -0400

    minor

--- preprocessData_Albeanu.m
+++ preprocessData_Albeanu.m
@@ -85,6 +85,8 @@ Nbatch      = ceil(d.bytes/2/NchanTOT /(NT-ops.ntbuff));
 Nbatch_buff = floor(4/5 * nint16s/rez.ops.Nchan /(NT-ops.ntbuff)); % factor of 4/5 for storing PCs of spikes
 Nbatch_buff = min(Nbatch_buff, Nbatch);
 
+ValidChannels = ops.ValidChannels(1:ops.Nchan);
+
 %% load data into patches, filter, compute covariance
 if isfield(ops,'fslow')&&ops.fslow<ops.fs/2
     [b1, a1] = butter(3, [ops.fshigh/ops.fs,ops.fslow/ops.fs]*2, 'bandpass');
@@ -254,9 +256,14 @@ for ibatch = 1:Nbatch
             datr = flipud(datr);
             datr = filter(b1, a1, datr);
             datr = flipud(datr);
+            
+            if ops.CAR
+                datr = datr - mean(datr(:,find(ValidChannels)),2);
+            end
         else
             datr = dataRAW;
         end
+        
         datr = datr(ioffset + (1:NT),:);
     end
     
